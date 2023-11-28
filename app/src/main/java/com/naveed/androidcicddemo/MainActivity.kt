d7e130c7c8e6725f198d025d5f4c3e40cd8d67cf
Muhammad Naveed <83871075+Naveed-Baloch@users.noreply.github.com>
2023-11-28 16:50:28 +0500

    Update MainActivity.kt

--- app/src/main/java/com/naveed/androidcicddemo/MainActivity.kt
+++ app/src/main/java/com/naveed/androidcicddemo/MainActivity.kt
@@ -15,7 +15,7 @@ import com.naveed.androidcicddemo.ui.theme.AndroidCICDDemoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent1 {
+        setContent {
             AndroidCICDDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
